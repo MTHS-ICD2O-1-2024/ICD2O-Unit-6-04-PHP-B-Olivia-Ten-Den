e1661cbcccfbbbd5e3f5c997fda5c29daf7d1793
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-29 22:12:17 +0000

    added math

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Area of Triangle, in PHP" />
+  <meta name="description" content="Volume of a Sphere, with query strings in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Mr. Coxall" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,12 +13,12 @@
     href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link
     rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+    href="https://code.getmdl.io/1.3.0/material.deep_purple-pink.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Area of Triangle, in PHP</title>
+  <title>Volume of a Sphere, with query strings in PHP</title>
 </head>
 
 <body>
@@ -26,7 +26,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Area of Triangle, in PHP</span>
+        <span class="mdl-layout-title">Volume of a Sphere, with query strings in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
@@ -34,11 +34,25 @@
         <img src="./images/sphere.png" alt="image of a sphere with it's radius labeled" />
       </div>
       <br />
-      <div class="page-content">Enter the base and height in cm.</div>
+      <div class="page-content">Enter the radius in cm.</div>
       <br />
       <div class="page-content-answer">
-        <div id="area">
-          The area of the triangle is 2 cm². </div>
+        <?php
+        echo $radius = $_GET['radius'];
+
+        if ($radius == null) {
+          $radius = 0;
+        }
+
+        $volume = 4 / 3 * M_PI * $radius ** 3;
+        $roundedVolume = round($volume, 2);
+
+        echo "<p>If the radius = " . $radius . " cm</p>";
+        echo "<p>The volume is: " . $roundedVolume . "cm3</p>";
+
+        ?>
+        <div id="volume">
+        </div>
       </div>
     </main>
   </div>
