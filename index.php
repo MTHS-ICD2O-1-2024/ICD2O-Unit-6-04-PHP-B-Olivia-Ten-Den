f97c8bfb323bce42a8e727906dfea19c75baecb6
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-29 21:40:35 +0000

    added code

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Area of Triangle, in PHP" />
+  <meta name="keywords" content="mths, ics2o" />
+  <meta name="author" content="Mr. Coxall" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href=".ko/css/style.css" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Area of Triangle, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Area of Triangle, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/sphere.png" alt="image of a sphere with it's radius labeled" />
+      </div>
+      <br />
+      <div class="page-content">Enter the base and height in cm.</div>
+      <br />
+      <div class="page-content-answer">
+        <div id="area">
+          The area of the triangle is 2 cm². </div>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
